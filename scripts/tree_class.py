3e944f36f110fa683b014f1d4b9bd635f6c50029
timsliu <timliu92@gmail.com>
2020-07-12 18:20:47 -0700

    started skeleton for tree_class

--- scripts/tree_class.py
+++ scripts/tree_class.py
@@ -3,17 +3,51 @@
 class tree_tex():
     def __init__(self, name):
         self.name = name
-        self.images = []       # list of images included
-        self.tex = ""          # contents of .tex file
+        self.images = []            # list of images included
+        self.tex = ""               # contents of .tex file
         self.last_edited = get_time #
         self.last_compiled = None
+        self.has_url = False       # link to Stanford tree encyclopedia
+        self.locations = []
 
-    def set_images(image_list):
-        self.images = image_list
+        self.read_template()        # read contents from template tex
+    
+    def read_template(self):
+        '''read the template tex file'''
 
     def read_tex(self):
         # TODO read the tex file and get the contents
         # as a list of strings
+
+    def update_tex(self):
+        '''updates the tex field of this object; does write out
+        the changes to the tex file'''
+
+        # read in the tex if there is one; none indicates new file
+
+        # update images
+
+        # update link
+
+        # update locations 
+
+
+    def write_tex(self):
+        '''writes out the tex field to a tex file'''
+
+        # archive the previous tex
+        # write to a new tex file
+        # update the last_edited date
+
+        self.mark_edited()
+
+    def mark_edited(self):
+        '''mark that the tex file has been edited'''
+    
+    def compile_tex(self):
+        '''compile the tex file'''
+
+        self.mark_compiled()
 
     def mark_compiled(self):
         # updates the last compiled time
